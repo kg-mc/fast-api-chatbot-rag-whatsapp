6f9d7485b4f529624d230389b44655787a226ebf
kg-mc <kleingmamanic@gmail.com>
2026-03-24 11:39:29 -0500

    requeriments3

--- factory/embbedings_factory.py
+++ factory/embbedings_factory.py
@@ -1,14 +1,10 @@
 from abc import ABC, abstractmethod
 from openai import OpenAI
-from config import EMBEDDINGS_MODEL_NAME_HF, EMBEDDINGS_MODEL_NAME_GEMINI
-from sentence_transformers import SentenceTransformer
+from config import EMBEDDINGS_MODEL_NAME_GEMINI
 from google.genai import Client
-import torch
 
 
 class EmbeddingsService(ABC):
-    model: SentenceTransformer
-    instruction: str
     @abstractmethod
     def generate(self):
         pass
@@ -17,22 +13,6 @@ class EmbeddingsService(ABC):
     def embed_query(self, query: str) -> list[float]:
         pass
 
-class HuggingFaceEmbeddingsService(EmbeddingsService):
-    def __init__(self, model_name: str):
-        self.model_name = model_name
-        # Aquí podrías cargar el modelo de Hugging Face si es necesario
-    def generate(self):
-        if self.model_name == "BAAI/bge-m3":
-            self.model = SentenceTransformer("BAAI/bge-m3")
-            self.instruction = "Represent this question for retrieving supporting documents: "
-        if self.model is None:
-            raise ValueError("Modelo de embeddings no inicializado")
-    def embed_query(self, query: str) -> list[float]:
-        embedding = self.model.encode(
-            [self.instruction + query],
-            normalize_embeddings=True
-        )
-        return embedding[0].tolist()
     
 class GeminiEmbeddingsService(EmbeddingsService):
     def __init__(self, model_name: str):
@@ -45,13 +25,13 @@ class GeminiEmbeddingsService(EmbeddingsService):
             model=self.model_name,
             contents=query
         )
-        
-        if not response.embeddings:
+        embeddings = response.embeddings
+        if not embeddings:
             raise ValueError("No se generó embedding para la query")
-        
-        embedding_vector = torch.tensor(response.embeddings[0].values)
-        
-        return embedding_vector.tolist()
+        values = embeddings[0].values
+        if values is None:
+            raise ValueError("Embedding inválido")
+        return values
 
 class OpenAIEmbeddingsService(EmbeddingsService):
     def __init__(self, model_name: str):
@@ -75,11 +55,7 @@ class OpenAIEmbeddingsService(EmbeddingsService):
 class EmbeddingsFactory:
     @staticmethod
     def create_embeddings_service(embeddings_type: str, model_name: str = "") -> EmbeddingsService:
-        if embeddings_type == "huggingface":
-            if model_name == "":
-                model_name = str(EMBEDDINGS_MODEL_NAME_HF)
-            return HuggingFaceEmbeddingsService(model_name)
-        elif embeddings_type == "gemini":
+        if embeddings_type == "gemini":
             if model_name == "":
                 model_name = str(EMBEDDINGS_MODEL_NAME_GEMINI)
             return GeminiEmbeddingsService(model_name)
